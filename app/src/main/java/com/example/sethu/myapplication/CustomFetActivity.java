82c754383f59c36bea27a8e32628c43863372022
sethusuresh <sethusuresh.ekm@gmail.com>
2018-07-15 23:59:39 +0530

    committing cust config changes

--- app/src/main/java/com/example/sethu/myapplication/CustomFetActivity.java
+++ app/src/main/java/com/example/sethu/myapplication/CustomFetActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -16,15 +17,18 @@ import com.example.sethu.myapplication.DTO.CustomConfigDTO;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import Util.TimeUtil;
+import ca.antonious.materialdaypicker.MaterialDayPicker;
 
 public class CustomFetActivity extends Activity{
 
     CustomConfigDTO customConfigDTO = new CustomConfigDTO();
+    BasicConfigDTO basicConfigDTO = new BasicConfigDTO();
     TimeUtil timeUtil = new TimeUtil();
 
     @Override
@@ -34,27 +38,118 @@ public class CustomFetActivity extends Activity{
         List<String> hourList = timeUtil.getHourList();
         List<String> minuteList = timeUtil.getMinuteList();
 
+        //get existing config
+        Map<String,Integer> userSelectedTime = getUserSelectedTime();
+
+        //setting days of week
+        MaterialDayPicker materialDayPicker = findViewById(R.id.day_picker);
+        materialDayPicker.setSelectedDays(getUserSelectedDaysOfWeek());
+
         //setting morning hr drop-down
         Spinner morningHourSpinner = findViewById(R.id.cust_mor_hr_spinner);
         ArrayAdapter<String> hourAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, hourList);
         morningHourSpinner.setAdapter(hourAdapter);
-        morningHourSpinner.setSelection(userSelectedTime.get(String.valueOf(R.string.basic_fet_mor_hr)));
+        morningHourSpinner.setSelection(userSelectedTime.get(String.valueOf(R.string.cust_fet_mor_hr)));
 
         //setting morning min drop-down
         Spinner morningMinuteSpinner = findViewById(R.id.cust_mor_min_spinner);
         ArrayAdapter<String> minuteAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, minuteList);
         morningMinuteSpinner.setAdapter(minuteAdapter);
-        morningMinuteSpinner.setSelection(userSelectedTime.get(String.valueOf(R.string.basic_fet_mor_min)));
+        morningMinuteSpinner.setSelection(userSelectedTime.get(String.valueOf(R.string.cust_fet_mor_min)));
 
         //setting evening hr drop-down
         Spinner eveningHourSpinner = findViewById(R.id.cust_eve_hr_spinner);
         eveningHourSpinner.setAdapter(hourAdapter);
-        eveningHourSpinner.setSelection(userSelectedTime.get(String.valueOf(R.string.basic_fet_eve_hr)));
+        eveningHourSpinner.setSelection(userSelectedTime.get(String.valueOf(R.string.cust_fet_eve_hr)));
 
         //setting evening min drop-down
         Spinner eveningMinuteSpinner = findViewById(R.id.cust_eve_min_spinner);
         eveningMinuteSpinner.setAdapter(minuteAdapter);
-        eveningMinuteSpinner.setSelection(userSelectedTime.get(String.valueOf(R.string.basic_fet_eve_min)));
+        eveningMinuteSpinner.setSelection(userSelectedTime.get(String.valueOf(R.string.cust_fet_eve_min)));
+    }
+
+    private Map<String,Integer> getUserSelectedTime() {
+        //fetching existing config
+        SharedPreferences sharedPref = getSharedPreferences("SmartGardnerData",Context.MODE_PRIVATE);
+        String daysOfWeek = sharedPref.getString(getString(R.string.cust_fet_days_of_week), getString(R.string.cust_fet_days));
+        String morningTime = sharedPref.getString(getString(R.string.cust_fet_morning_time), "01:00:00");
+        String eveningTime = sharedPref.getString(getString(R.string.cust_fet_evening_time), "01:00:00");
+        int morningHrPos = timeUtil.getHourList().indexOf(morningTime.split(":")[0].replaceFirst("^0+(?!$)", ""));
+        int morningMinPos = timeUtil.getMinuteList().indexOf(morningTime.split(":")[1].replaceFirst("^0+(?!$)", ""));
+        int eveningHrPos = timeUtil.getHourList().indexOf(eveningTime.split(":")[0].replaceFirst("^0+(?!$)", ""));
+        int eveningMinPos = timeUtil.getMinuteList().indexOf(eveningTime.split(":")[1].replaceFirst("^0+(?!$)", ""));
+        Map<String,Integer> timePosMap= new HashMap<>();
+        timePosMap.put(String.valueOf(R.string.cust_fet_mor_hr), morningHrPos);
+        timePosMap.put(String.valueOf(R.string.cust_fet_mor_min), morningMinPos);
+        timePosMap.put(String.valueOf(R.string.cust_fet_eve_hr), eveningHrPos);
+        timePosMap.put(String.valueOf(R.string.cust_fet_eve_min), eveningMinPos);
+
+        //show user selection
+        TextView selectedBasicConfig = findViewById (R.id.selected_cust_config);
+        selectedBasicConfig.setText(daysOfWeek+"\nMORNING: "+ morningTime + " am\n" + "EVENING: " + eveningTime +" pm");
+
+        return timePosMap;
+    }
+
+    private List<MaterialDayPicker.Weekday> getUserSelectedDaysOfWeek(){
+        SharedPreferences sharedPref = getSharedPreferences("SmartGardnerData",Context.MODE_PRIVATE);
+        String daysOfWeek = sharedPref.getString(getString(R.string.cust_fet_days_of_week), getString(R.string.cust_fet_days));
+        List<String> selectedDaysOfWeek = Arrays.asList(daysOfWeek.split(","));
+        List<MaterialDayPicker.Weekday> daysSelected = new ArrayList<>();
+        for(String selectedDay : selectedDaysOfWeek){
+            if("SUNDAY".equalsIgnoreCase(selectedDay)){
+                daysSelected.add(MaterialDayPicker.Weekday.SUNDAY);
+            }
+            else if("MONDAY".equalsIgnoreCase(selectedDay)){
+                daysSelected.add(MaterialDayPicker.Weekday.MONDAY);
+            }
+            else if("TUESDAY".equalsIgnoreCase(selectedDay)){
+                daysSelected.add(MaterialDayPicker.Weekday.TUESDAY);
+            }
+            else if("WEDNESDAY".equalsIgnoreCase(selectedDay)){
+                daysSelected.add(MaterialDayPicker.Weekday.WEDNESDAY);
+            }
+            else if("THURSDAY".equalsIgnoreCase(selectedDay)){
+                daysSelected.add(MaterialDayPicker.Weekday.THURSDAY);
+            }
+            else if("FRIDAY".equalsIgnoreCase(selectedDay)){
+                daysSelected.add(MaterialDayPicker.Weekday.FRIDAY);
+            }
+            else if("SATURDAY".equalsIgnoreCase(selectedDay)){
+                daysSelected.add(MaterialDayPicker.Weekday.SATURDAY);
+            }
+        }
+        return daysSelected;
+    }
+
+    public void saveCustConfig(View view) throws ParseException {
+        Spinner morningHourSpinner = findViewById(R.id.cust_mor_hr_spinner);
+        Spinner morningMinuteSpinner = findViewById(R.id.cust_mor_min_spinner);
+        Spinner eveningHourSpinner = findViewById(R.id.cust_eve_hr_spinner);
+        Spinner eveningMinuteSpinner = findViewById(R.id.cust_eve_min_spinner);
+        basicConfigDTO.setMorning_hour(morningHourSpinner.getSelectedItem().toString());
+        basicConfigDTO.setMorning_minute(morningMinuteSpinner.getSelectedItem().toString());
+        basicConfigDTO.setEvening_hour(eveningHourSpinner.getSelectedItem().toString());
+        basicConfigDTO.setEvening_minute(eveningMinuteSpinner.getSelectedItem().toString());
+        TextView selectedBasicConfig = findViewById (R.id.selected_cust_config);
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm a");
+        java.sql.Time morningTime = new java.sql.Time(formatter.parse(basicConfigDTO.getMorning_hour()+":"+basicConfigDTO.getMorning_minute()+" am").getTime());
+        basicConfigDTO.setMorningTime(morningTime);
+        java.sql.Time eveningTime = new java.sql.Time(formatter.parse(basicConfigDTO.getEvening_hour()+":"+basicConfigDTO.getEvening_minute()+" pm").getTime());
+        basicConfigDTO.setEveningTime(eveningTime);
+        customConfigDTO.setBasicConfigDTO(basicConfigDTO);
+        MaterialDayPicker materialDayPicker = findViewById(R.id.day_picker);
+        List<MaterialDayPicker.Weekday> daysSelected = materialDayPicker.getSelectedDays();
+        customConfigDTO.setDay(TextUtils.join(",",daysSelected));
+        selectedBasicConfig.setText(customConfigDTO.getDay()+"\nMORNING: "+ basicConfigDTO.getMorningTime() + " am\n" + "EVENING: " + basicConfigDTO.getEveningTime() +" pm");
+        //save to local storage
+        SharedPreferences sharedPref = getSharedPreferences("SmartGardnerData",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.cust_fet_morning_time), customConfigDTO.getBasicConfigDTO().getMorningTime().toString());
+        editor.putString(getString(R.string.cust_fet_evening_time), customConfigDTO.getBasicConfigDTO().getEveningTime().toString());
+        editor.putString(getString(R.string.cust_fet_days_of_week), customConfigDTO.getDay().toString());
+        editor.apply();
+        Toast.makeText(this, "new timings saved successfully!!!", Toast.LENGTH_SHORT).show();
     }
 
 
--- app/src/main/java/com/example/sethu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sethu/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.sethu.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +20,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveConfig(View view){
+        String morningTime = null;
+        String eveningTime = null;
+        String daysOfWeek = null;
+        boolean canProceed = true;
         RadioGroup radioGroup = findViewById(R.id.radioGroup);
-        //if(getString(radioGroup.getCheckedRadioButtonId()) == "")
+        int selectedRadioId = radioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton = findViewById(selectedRadioId);
+        int position = radioGroup.indexOfChild(radioButton);
+        if(position == 0){
+            //Daily
+            SharedPreferences sharedPref = getSharedPreferences("SmartGardnerData", Context.MODE_PRIVATE);
+            morningTime = sharedPref.getString(getString(R.string.basic_fet_morning_time), "01:00:00");
+            eveningTime = sharedPref.getString(getString(R.string.basic_fet_evening_time), "01:00:00");
+            daysOfWeek = getString(R.string.cust_fet_days);
+            canProceed = true;
+        }
+        else if(position == 1){
+            //Cust
+            SharedPreferences sharedPref = getSharedPreferences("SmartGardnerData",Context.MODE_PRIVATE);
+            daysOfWeek = sharedPref.getString(getString(R.string.cust_fet_days_of_week), getString(R.string.cust_fet_days));
+            morningTime = sharedPref.getString(getString(R.string.cust_fet_morning_time), "01:00:00");
+            eveningTime = sharedPref.getString(getString(R.string.cust_fet_evening_time), "01:00:00");
+            canProceed = true;
+        }
+        else if(position == 2){
+            //Adv
+            TextView selectedBasicConfig = findViewById (R.id.selected_config);
+            selectedBasicConfig.setText("");
+            Toast.makeText(this, "Coming soon!!!", Toast.LENGTH_SHORT).show();
+            canProceed = false;
+        }
+        else{
+            canProceed = false;
+            Toast.makeText(this, "Please select an option!!!", Toast.LENGTH_SHORT).show();
+        }
+
+        if(canProceed){
+            //show user selection
+            TextView selectedBasicConfig = findViewById (R.id.selected_config);
+            selectedBasicConfig.setText(daysOfWeek+"\nMORNING: "+ morningTime + " am\n" + "EVENING: " + eveningTime +" pm");
+            //sending to arduino
+        }
     }
 
     public void dailyConfig(View view){
